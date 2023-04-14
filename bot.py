9d20ca5d5935f92882509923aa5c77b8f58e779a
Cory Tabor <tabeman3906@hotmail.com>
2023-04-14 09:42:38 -0500

    added an extra ammo website and returns top 10 of combined sites

--- bot.py
+++ bot.py
@@ -24,20 +24,58 @@ HEADERS = {
 
 
 class FatherBot:
-    def __init__(self, url, headers):
-        self.url = url
+    """
+    A base class for web scraping bots that crawl websites and generate daily deals reports in PDF format.
+    """
+
+    def __init__(self, urls, headers=None):
+        """
+        Initializes a new instance of the FatherBot class.
+
+        Args:
+            url (str): The URL of the website to crawl.
+            headers (dict): A dictionary of HTTP headers to send with each request.
+        """
+        self.urls = urls
         self.headers = headers
         self.results = []
 
-    def generate_pdf(self):
-        with open("pdf.html") as file:
+    def search(self):
+        """
+        This method must be implemented by any child class that inherits from FatherBot.
+        It should scrape data from a website and populate the self.results list with the results.
+        """
+        raise NotImplementedError
+
+    def generate_pdf(self, pdf_name):
+        """
+        Generates a PDF report of the search results.
+
+        Args:
+            pdf_name (str): The name of which PDF HTML to open.
+
+        Returns:
+            str: The name of the generated PDF file.
+        """
+        # Open the HTML template file and render the results
+        with open(f"{pdf_name}_pdf.html") as file:
             template = Template(file.read())
         html = template.render(results=self.results)
+
+        # Generate PDF from HTML
         pdf_file = "daily_deals.pdf"
         HTML(string=html).write_pdf(pdf_file)
         return pdf_file
 
     def send_email(self, subject, message, attachment):
+        """
+        Sends an email message with the daily deals report attached.
+
+        Args:
+            subject (str): The subject line of the email.
+            message (str): The body of the email message.
+            attachment (str): The path to the PDF file to attach to the email.
+        """
         recipients = ["tabeman3906@hotmail.com"]
         email = config("GMAIL_EMAIL")
         password = config("GMAIL_PASSWORD")
@@ -46,11 +84,15 @@ class FatherBot:
         msg["From"] = email
         msg["To"] = ", ".join(recipients)
         msg.attach(MIMEText(message))
+
+        # Attach the PDF to the email
         with open(attachment, "rb") as file:
             attachment_data = file.read()
         attachment_part = MIMEApplication(attachment_data, Name="daily_deals.pdf")
         attachment_part["Content-Disposition"] = f"attachment; filename=daily_deals.pdf"
         msg.attach(attachment_part)
+
+        # Send the email
         with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
             smtp.ehlo()
             smtp.starttls()
@@ -60,9 +102,12 @@ class FatherBot:
             print("Email sent successfully!")
 
     def run(self):
+        """
+        Runs the bot to crawl the website, generate the daily deals report, and send it via email.
+        """
         try:
-            self.search()
-            pdf_file = self.generate_pdf()
+            pdf_name = self.search()
+            pdf_file = self.generate_pdf(pdf_name)
             self.send_email(
                 "Daily Deals Results",
                 "We've crawled the web and found the best deals for you! Please see the attached PDF for the results.",
@@ -73,12 +118,28 @@ class FatherBot:
 
 
 class NeweggDealsBot(FatherBot):
+    """
+    A web scraping bot that crawls newegg.com for daily deals and generates a report in PDF format.
+
+    Inherits from the FatherBot class.
+    """
+
     def search(self):
-        page = requests.get(self.url, headers=self.headers)
+        """
+        Scrapes newegg.com for daily deals and populates the results list.
+
+        Returns:
+            str: The name of the website that was searched.
+        """
+        # Send a GET request to the website and parse the HTML content with BeautifulSoup
+        page = requests.get(self.urls, headers=self.headers)
         soup = BeautifulSoup(page.content, "html.parser")
+
+        # Find the section of the page with the daily deals and loop through each item
         inner = soup.find("div", {"class": "item-cells-wrap tile-cells five-cells"})
         for row in inner.find_all("div", {"class": "item-cell"}):
             result = {}
+            # Extract the relevant information for each deal and add it to the results dictionary
             result["title"] = row.find("a", {"class": "item-title"}).text.strip()
             result["link"] = row.find("a", {"class": "item-title"}).get("href")
             result["price_was"] = row.find("li", {"class": "price-was"}).text.strip()
@@ -93,13 +154,43 @@ class NeweggDealsBot(FatherBot):
             ).text.strip()
             self.results.append(result)
 
+        # Return name of website for use in the generate_pdf() method
+        return "newegg"
+
 
 class AmmoDealsBot(FatherBot):
+    """
+    A web scraping bot that crawls targetsportsusa.com and palmettostatearmory.com for ammo deals.
+
+    Inherits from the FatherBot class.
+    """
+
     def search(self):
+        """
+        Executes web scraping on each URL in self.urls, then sorts the results by cpr and returns "ammo".
+        """
+        for url in self.urls:
+            if "targetsportsusa.com" in url:
+                self.scrape_target_sports(url)
+            if "palmettostatearmory.com" in url:
+                self.scrape_palmetto(url)
+
+        self.results.sort(key=lambda x: int(x["cpr"]))
+        self.results = self.results[:10]
+
+        # Return name of website for use in the generate_pdf() method
+        return "ammo"
+
+    def scrape_palmetto(self, url):
+        """
+        Scrapes palmettostatearmory.com for ammo deals.
+
+        Appends a dictionary containing the title, link, price, and cpr of each deal to self.results.
+        """
         driver = Chrome()
 
         # Navigate to the page
-        driver.get(self.url)
+        driver.get(url)
         soup = BeautifulSoup(driver.page_source, "html.parser")
 
         inner = soup.find("ol", {"class": "products list items product-items"})
@@ -109,23 +200,78 @@ class AmmoDealsBot(FatherBot):
             result["link"] = row.find("a", {"class": "product-item-link"}).get("href")
             prices = row.find_all("span", {"class": "price"})
             if len(prices) == 3:
+                # If there are three prices, the second is the actual price and the third is the cpr.
                 result["price"] = prices[1].text.strip()
-                result["cpr"] = prices[2].text.strip()
+                result["cpr"] = prices[2].text.strip().replace("$0.", "")
             else:
+                # If there are only two prices, the first is the actual price and the second is the cpr.
                 result["price"] = prices[0].text.strip()
-                result["cpr"] = prices[1].text.strip()
+                result["cpr"] = prices[1].text.strip().replace("$0.", "")
             self.results.append(result)
+
+        driver.quit()
+
+    def scrape_target_sports(self, url):
+        """
+        Scrapes targetsportsusa.com for ammo deals.
+
+        Appends a dictionary containing the title, link, price, and cpr of each deal to self.results.
+        """
+        driver = Chrome()
+
+        # Navigate to the page
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+
+        inner = soup.find("div", {"class": "ResultsArea"}).find(
+            "ul", {"class": "product-list"}
+        )
+        for row in inner.find_all("li")[:15]:
+            result = {}
+            result["title"] = row.find("h2").text.strip()
+            link = row.find("a").get("href")
+            result["link"] = f"https://www.targetsportsusa.com{link}"
+            prices = list(
+                filter(
+                    lambda x: x != "",
+                    row.find("div", {"class": "product-listing-price"})
+                    .text.strip()
+                    .rsplit("$", 1),
+                )
+            )
+            if len(prices) == 2:
+                # If there are two prices, the first is the actual price and the second is the cpr.
+                result["price"] = prices[0].strip()
+                result["cpr"] = (
+                    prices[1]
+                    .strip()
+                    .split(" ")[0]
+                    .replace("0.", "")
+                    .replace("0", "", 1)
+                )
+                self.results.append(result)
 
         driver.quit()
 
 
 def main():
-    palmetto_url = "https://palmettostatearmory.com/9mm-ammo.html?caliber_multi=9mm"
-    newegg_bot = NeweggDealsBot(
-        "https://www.newegg.com/todays-deals?cm_sp=Head_Navigation-_-Under_Search_Bar-_-Today%27s+Best+Deals&icid=720202",
-        HEADERS,
+    answer = (
+        input("Which site would you like to crawl? (newegg or ammo) ").lower().strip()
     )
-    newegg_bot.run()
+    if answer == "newegg":
+        newegg_bot = NeweggDealsBot(
+            "https://www.newegg.com/todays-deals?cm_sp=Head_Navigation-_-Under_Search_Bar-_-Today%27s+Best+Deals&icid=720202",
+            HEADERS,
+        )
+        newegg_bot.run()
+    else:
+        palmetto_url = "https://palmettostatearmory.com/9mm-ammo.html?caliber_multi=9mm"
+        targets_sports_ammo_url = (
+            "https://www.targetsportsusa.com/9mm-luger-ammo-c-51.aspx"
+        )
+        urls = [targets_sports_ammo_url, palmetto_url]
+        ammo_bot = AmmoDealsBot(urls, HEADERS)
+        ammo_bot.run()
 
 
 if __name__ == "__main__":
--- ss.py
+++ ss.py
@@ -9,16 +9,39 @@ import time
 
 
 class AmmoDealsBot:
-    def __init__(self, url, headers):
-        self.url = url
+    def __init__(self, urls, headers):
+        self.urls = urls
         self.headers = headers
         self.results = []
 
     def search(self):
+        """
+        Executes web scraping on each URL in self.urls, then sorts the results by cpr and returns "ammo".
+        """
+        for url in self.urls:
+            if "targetsportsusa.com" in url:
+                self.scrape_target_sports(url)
+            if "palmettostatearmory.com" in url:
+                self.scrape_palmetto(url)
+
+        self.results.sort(key=lambda x: int(x["cpr"]))
+        self.results = self.results[:10]
+
+        print(self.results)
+
+        # Return name of website for use in the generate_pdf() method
+        return "ammo"
+
+    def scrape_palmetto(self, url):
+        """
+        Scrapes palmettostatearmory.com for ammo deals.
+
+        Appends a dictionary containing the title, link, price, and cpr of each deal to self.results.
+        """
         driver = Chrome()
 
         # Navigate to the page
-        driver.get(self.url)
+        driver.get(url)
         soup = BeautifulSoup(driver.page_source, "html.parser")
 
         inner = soup.find("ol", {"class": "products list items product-items"})
@@ -28,18 +51,64 @@ class AmmoDealsBot:
             result["link"] = row.find("a", {"class": "product-item-link"}).get("href")
             prices = row.find_all("span", {"class": "price"})
             if len(prices) == 3:
+                # If there are three prices, the second is the actual price and the third is the cpr.
                 result["price"] = prices[1].text.strip()
-                result["cpr"] = prices[2].text.strip()
+                result["cpr"] = prices[2].text.strip().replace("$0.", "")
             else:
+                # If there are only two prices, the first is the actual price and the second is the cpr.
                 result["price"] = prices[0].text.strip()
-                result["cpr"] = prices[1].text.strip()
+                result["cpr"] = prices[1].text.strip().replace("$0.", "")
             self.results.append(result)
+
+        driver.quit()
+
+    def scrape_target_sports(self, url):
+        """
+        Scrapes targetsportsusa.com for ammo deals.
+
+        Appends a dictionary containing the title, link, price, and cpr of each deal to self.results.
+        """
+        driver = Chrome()
+
+        # Navigate to the page
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+
+        inner = soup.find("div", {"class": "ResultsArea"}).find(
+            "ul", {"class": "product-list"}
+        )
+        for row in inner.find_all("li")[:15]:
+            result = {}
+            result["title"] = row.find("h2").text.strip()
+            link = row.find("a").get("href")
+            result["link"] = f"https://www.targetsportsusa.com{link}"
+            prices = list(
+                filter(
+                    lambda x: x != "",
+                    row.find("div", {"class": "product-listing-price"})
+                    .text.strip()
+                    .rsplit("$", 1),
+                )
+            )
+            if len(prices) == 2:
+                # If there are two prices, the first is the actual price and the second is the cpr.
+                result["price"] = prices[0].strip()
+                result["cpr"] = (
+                    prices[1]
+                    .strip()
+                    .split(" ")[0]
+                    .replace("0.", "")
+                    .replace("0", "", 1)
+                )
+                self.results.append(result)
 
         driver.quit()
 
 
 def main():
-    url = "https://palmettostatearmory.com/9mm-ammo.html?caliber_multi=9mm"
+    palmetto_url = "https://palmettostatearmory.com/9mm-ammo.html?caliber_multi=9mm"
+    targets_sports_ammo_url = "https://www.targetsportsusa.com/9mm-luger-ammo-c-51.aspx"
+    urls = [palmetto_url, targets_sports_ammo_url]
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -49,8 +118,10 @@ def main():
         "Upgrade-Insecure-Requests": "1",
     }
 
-    bot = AmmoDealsBot(url, headers)
+    bot = AmmoDealsBot(urls, headers)
     bot.search()
+    # top_ten = bot.get_top_ten_deals()
+    # print(top_ten)
 
 
 if __name__ == "__main__":
